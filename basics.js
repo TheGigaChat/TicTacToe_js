c7cac5d22aab7cf495d3fcabf58fb1852c36150f
Artur Tudeng <ardzek@taltech.ee>
2024-11-26 21:34:34 +0200

    First tutorial notes and code

---
+++ basics.js
@@ -0,0 +1,10 @@
+"use strict"  // promote good habits (foundation)
+// First programm
+// console.log("Hello world!")
+// console.log("Hi, how are you?")
+
+// Variables
+let x = "Hi, how are you?"  // 'I'm creating a variable I can change.'
+console.log(x)
+x = 1
+console.log(x)
