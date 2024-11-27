a0917a10b5cb4b6fe89124a91f56aaed87c39787
Artur Tudeng <ardzek@taltech.ee>
2024-11-27 21:53:08 +0200

    if statements topic + the challenge rules

--- basics.js
+++ basics.js
@@ -3,6 +3,7 @@
 // console.log("Hello world!")
 // console.log("Hi, how are you?")
 
+
 // Variables
 // let x  // (initialisation or init) block in our memory
 // x = "Hi, how are you?"  // 'I'm creating a variable I can change.' (declaration)
@@ -11,4 +12,47 @@
 // x = 1  // (reasignment)
 // console.log(x)
 
-//
+
+// Basic Math
+// console.log(2 + 2)  // number type, rules are as in the Math
+// console.log(2 - 2)  
+// console.log(2 * 2)  
+// console.log(2 / 2)  
+
+// let a = 1
+// let b = 2
+// let formula = a + b
+// console.log(formula)
+
+// challenge
+// let myFormula // parameter a, b, c => sum of a and c + b multiplies c
+
+
+// If statements
+// let statement = true
+// statement = false
+
+//example
+let light = true
+let personInRoom = true
+
+// if (personInRoom === true) {  // common "=" in the math, comparison. true === true => true
+//     light = true
+// }
+
+// if (personInRoom === false) {  // true === false => false
+//     light = false
+// }
+
+// if (personInRoom === true) {
+//     light = true
+// } else {  // if first statement = false, then else statement = true
+//     light = false
+// }
+
+// challenge
+let engineWorks = true
+let personInCar = true
+let carLock = false
+
+// rules. If person in car => turn off the carLock and start the engine, oterwise turn on the carLock and turn off the engine. If engineWorks => type in the console the sentence "Brum-brum. I am ready!"
