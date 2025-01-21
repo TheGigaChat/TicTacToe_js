2808863c5e0f432b1147fafb2dc71817ae4cb99b
Artur Tudeng <ardzek@taltech.ee>
2025-01-21 15:41:53 +0200

    recording_test01

--- recordingTests.js
+++ recordingTests.js
@@ -1,12 +1,24 @@
 "use strict"
 
-// console.log("My name is Dave.")  // string
+// math
 
-// let myDogsName = "Drake"
-// console.log(myDogsName)
+// addition
+let x = 10
+let y = 8
+let sumOfTwoNumbers = x + y
+console.log(sumOfTwoNumbers)
 
-    // let myGrade = 5
-    // console.log(myGrade)
+let name = 'Ethan'  // string
+let surename = 'Evans'
+let nameAndSurename = name + surename
+console.log(nameAndSurename)
 
-    // myGrade = 4
-    // console.log(myGrade)
+// code challange
+// create a formula that will add x and z and substract y from the result. Take all the variables from the user (using prompt message).
+
+
+x = 100
+y = 50
+let z = 40
+let myFormula = x + z - y
+console.log(myFormula)
