bce450fc473e2d568833310c9c011e2076172cff
Artur Tudeng <ardzek@taltech.ee>
2025-01-21 19:56:19 +0200

    code delay test

--- recordingTests.js
+++ recordingTests.js
@@ -17,8 +17,31 @@ console.log(nameAndSurename)
 // create a formula that will add x and z and substract y from the result. Take all the variables from the user (using prompt message).
 
 
-x = 100
-y = 50
-let z = 40
-let myFormula = x + z - y
-console.log(myFormula)
+// x = 100
+// y = 50
+// let z = 40
+// let myFormula = x + z - y
+// console.log(myFormula)
+
+// console.log(78 - 8)
+// console.log(292 - 282)
+// console.log(292 - 282)
+// console.log(292 - 282)
+// let xa = 'akdfkf'
+
+// xad = 'My name is Arthur.'
+// console.log(xad)
+
+// very bright, but I can use it
+// darker screen doesn't mean worsen screen
+// even a new coded doesn't take so much energy and resourses from my PC
+
+let add = "words from the bank"
+// I hope
+// I don't know 
+
+// let's code something
+// let's code something
+// code test delay
+
+// code test delay 
